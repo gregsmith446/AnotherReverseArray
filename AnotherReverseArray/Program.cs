650e1eead4aa1ee18ad597589cae9bf9ff8edc73
Greg Smith <gregsmith446@gmail.com>
2019-01-30 18:33:45 -0500

    different approach for reversing array

--- AnotherReverseArray/Program.cs
+++ AnotherReverseArray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace AnotherReverseArray
 {
@@ -6,7 +7,13 @@ namespace AnotherReverseArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("size of array - int");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter {0} space separated integers", n);
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            Console.WriteLine(string.Join(" ", arr.Reverse()));
         }
     }
 }
